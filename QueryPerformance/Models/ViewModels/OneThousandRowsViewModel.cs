b1f5447eb575f269a1dcbbc79bbd57dd4482cedd
Davi Pinto Ferreira <davipferr@gmail.com>
2025-04-20 16:54:48 -0300

    feat(Mappers, ViewModels): Update OneThousandRowsMapper to map from OneThousandRowsViewModel, enhancing data handling. Modify OneThousandRowsViewModel properties for type consistency and update view to utilize the new ViewModel for better data representation.

--- QueryPerformance/Models/ViewModels/OneThousandRowsViewModel.cs
+++ QueryPerformance/Models/ViewModels/OneThousandRowsViewModel.cs
@@ -17,14 +17,14 @@ namespace QueryPerformance.Models.ViewModels
         public string   Email         { get; set; }
         public string   Cep           { get; set; }
         public string   Endereco      { get; set; }
-        public string   Numero        { get; set; }
+        public int      Numero        { get; set; }
         public string   Bairro        { get; set; }
         public string   Cidade        { get; set; }
         public string   Estado        { get; set; }
         public string   TelefoneFixo  { get; set; }
         public string   Celular       { get; set; }
         public decimal  Altura        { get; set; }
-        public decimal  Peso          { get; set; }
+        public int  Peso          { get; set; }
         public string   TipoSanguineo { get; set; }
         public string   Cor           { get; set; }
     }
